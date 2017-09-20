51b77fc7bbf3c7816fa162a9d139fdddeb97a87f
jeffmon <jeffmon@gmail.com>
2017-09-19 23:37:53 -1000

    added register submit to database to create new user from LoginService to register controller. has an error in sequelize but still goes through

--- public/js/controllers/registerController.js
+++ public/js/controllers/registerController.js
@@ -1,6 +1,28 @@
 angular.module("myApp").controller("registerController", [
   "$scope",
-  function($scope) {
+  "LoginService",
+  "localStorageService",
+  "$window",
+  function($scope, LoginService, localStorageService, $window) {
     console.log("test");
+    $scope.user = null;
+    $scope.isNotLoggedIn = true;
+    $scope.isLoggedIn = false;
+
+    $scope.register = function(user) {
+      if ($scope.user.length > 0) {
+        LoginService.registerUser($scope.user)
+          .then(res => {
+            if (res) {
+              return localStorageService.set(res, "user");
+            }
+          })
+          .then(() => {
+            $window.location.href = "/home";
+          });
+      } else {
+        console.log(null);
+      }
+    };
   }
 ]);
--- public/js/services/loginService.js
+++ public/js/services/loginService.js
@@ -1,10 +1,29 @@
 angular.module("myApp").service("LoginService", [
   "$http",
-  function($http) {
+  "$httpParamSerializer",
+  function($http, $httpParamSerializer) {
     return {
       loginUser: function(user) {
         return $http.get(`/api/find/${user}`).then(function(res) {
           return res.data;
+        });
+      },
+      registerUser: function(user) {
+        return $http({
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+          },
+          url: "/api/users",
+          method: "POST",
+          transformRequest: $httpParamSerializer,
+          transformResponse: function(x) {
+            return angular.fromJson(angular.fromJson(x));
+          },
+          data: {
+            name: user
+          }
+        }).then(function(res) {
+          return res.data.name;
         });
       }
     };
